07ae849d7046990250004abfe212eee52654f751
ribadmilo <admilo.ribeiro@escolinhas.pt>
2013-04-01 23:12:22 +0100

    Fixed sintax error

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -11,7 +11,7 @@ class Interaction < ActiveRecord::Base
   total_trokaAki = Interaction.joins(:user =>:roles).where("interactions.user_id = '#{self.user_id}' AND roles.name ='vendor' AND date_trunc('month', interactions.created_at) = date_trunc('month', current_timestamp)").group("interactions.user_id").count
 	unless total_trokaAki.size == 0
 		if total_trokaAki.first.last >1000
-	 		MovercadoMailver.alert_email('ribadmilo@gmail.com',"User #{self.user_id} has too much activity. Verify it!")
+	 		MovercadoMailer.alert_email('ribadmilo@gmail.com',"User #{self.user_id} has too much activity. Verify it!")
 	 	end
 	end
   end
