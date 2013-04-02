793118d03117f5537c9467e8ab3def2a79bcd8df
ribadmilo <admilo.ribeiro@escolinhas.pt>
2013-04-02 14:45:05 +0100

    deliver message test

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,10 @@ require File.expand_path('../application', __FILE__)
 MovercadoAnalysis::Application.initialize!
 
 
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
