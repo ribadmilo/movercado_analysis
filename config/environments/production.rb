e9b9398221006447142d965ee884b96efcd8f52f
ribadmilo <admilo.ribeiro@escolinhas.pt>
2013-04-02 09:16:38 +0100

    Moved configurations of sendgrid to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,20 @@ MovercadoAnalysis::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'desolate-castle-6668.herokuapp.com' }
+  config.action_mailer.smtp_settings =  {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
