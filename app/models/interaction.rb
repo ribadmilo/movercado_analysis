b6942ea8ba5bbab941a1a1e8eb1bf2cf704f0417
ribadmilo <admilo.ribeiro@escolinhas.pt>
2013-04-04 09:23:46 +0100

    Fixing error in the message body and number of messages to send

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -10,7 +10,7 @@ class Interaction < ActiveRecord::Base
   
   total_trokaAki = Interaction.joins(:user =>:roles).where("interactions.user_id = '#{self.user_id}' AND roles.name ='vendor' AND date_trunc('month', interactions.created_at) = date_trunc('month', current_timestamp)").group("interactions.user_id").count
 	unless total_trokaAki.size == 0
-		if total_trokaAki.first.last > 1000 &&
+		if total_trokaAki.first.last > 1000 && total_trokaAki.first.last < 1005 #testing purposes
 	 		MovercadoMailer.alert_email('ribadmilo@gmail.com',"User, vendor, #{self.user_id} is having to much activity for this month. Verify it!").deliver
 	 	end
 	end
